0ad2950741ff5299b2bc4a26ba1bf0ee9e112a8f
1173860556@qq.com <1173860556@qq.com>
2017-06-07 17:39:08 +0800

    。

--- vue-chat/src/components/Responsive/index.vue
+++ vue-chat/src/components/Responsive/index.vue
@@ -1,59 +1,35 @@
 <template>
-<div class='container'>
-      <div class='row'>
-        <div class='col-lg-4'>
-          <h1>demo</h1>
-        </div>
-
-        <div class='col-lg-8'>
-          <div class='panel panel-default' style='min-width:500px;box-shadow:4px 4px 10px #888888;'>
-            <div class="panel-heading">
-              <button id='btnCreate'>
-                <span class="glyphicon glyphicon-plus"></span>
-              </button>
-              <span>&nbsp;&nbsp;&nbsp;</span>
-            </div>
-            <div class="panel-body">
-              <div style='float: left;width:100%'>
-
-                <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-                  Launch demo modal
-                </button>
-
-                <!-- Modal -->
-                <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-                      </div>
-                      <div class="modal-body">
-                        ...
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
+<div class="bg">
+  <div class="jumbotron">
+    <div class="container">
+      <h1>^&*()_</h1>
+      <p>...</p>
+      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
     </div>
+    </div>
+  </div>
+</div>
 </template>
+
+<style scoped>
+  .bg{
+    width: 100%;
+    height: 100%;
+    background: url('../../../static/imgs/bg.jpg') 100% 100% no-repeat;
+  }
+  .jumbotron{
+    height: 100%;
+    background: rgba(0,0,0,0.6);
+  }
+  .jumbotron p, .jumbotron h1{
+    color: #fff;
+  }
+</style>
 
 <script>
 	export default{
 	  data () {
-	    return {
-	      checked: true
-	    }
+	    return {}
 	  }
 	}
 </script>
